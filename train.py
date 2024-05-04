7d59d702856a163f43bb64a512c6c7b34f5f03dd
abdrafay <zainrafay12@gmail.com>
2024-05-04 23:08:05 +0500

    changes

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ import numpy as np
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', type=str, default='config/underwater.json',
+    parser.add_argument('-c', '--config', type=str, default='/kaggle/working/Underwater-Image-Enhancement-using-transformer-with-adaptive-skipping-mechanism/config/underwater.json',
                         help='JSON file for configuration')
     parser.add_argument('-p', '--phase', type=str, choices=['train', 'val'],
                         help='Run either train(training) or val(generation)', default='train')
